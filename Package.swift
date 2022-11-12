3e0a94d78ef0daebb1ad26e05967f7b28684ed74
Tun Cham Roeun <chamroeuntun25@outlook.com>
2022-11-12 19:26:01 +0700

    add sort(using)

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "swift-identified-collections",
+  platforms: [.iOS(.v13)],
   products: [
     .library(
       name: "IdentifiedCollections",
